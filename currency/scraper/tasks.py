3d715bd87bb3c7d6450979c585e38f7cb1616d04
t <ssstrz@gmail.com>
2018-04-18 10:32:04 +0200

    fix beat working time

--- currency/scraper/tasks.py
+++ currency/scraper/tasks.py
@@ -15,7 +15,7 @@ def scrap(lang):
 app.conf.beat_schedule = {
     'scrap_{}'.format(cur): {
         'task': 'scraper.tasks.scrap',
-        'schedule': crontab(minute=i%24, hour=8),
+        'schedule': crontab(minute=0, hour=i%24),
         'args': [cur],
     }
     for i, cur in enumerate(CUR_PATTERNS)
